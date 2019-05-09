64eb8a0edcd2eda47c9dc7531b18a076a8d1a833
David Schwartz <dmschwartz@bis.midco.net>
2019-05-09 16:03:39 -0500

    removed old analyze method, added test method
    analyze method needs more testing

--- Main.py
+++ Main.py
@@ -11,15 +11,26 @@ slide_window = SlidingWindow(rast.img.meta['height'], rast.img.meta['width'], 1)
 # print(rast.img.meta)
 
 # arr = np.array([[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16]])
-# arr = np.array([[1,1,1,0],[0,1,0,1],[0,0,1,1],[1,0,1,1]])
-img = rast.img.read(BandEnum.red.value )
+arr = np.random.randint(2, size=(7700,5650))
+img = rast.img.read(BandEnum.red.value)
 img = rast.create_binary_image(img, 127)
-img2 = slide_window.analyze_number_two(img, 'sum')
+print(img.shape[0])
+print(img.shape[1])
+img2 = slide_window.analyze(arr, 'sum')
+img3 = slide_window.analyze_test(arr, 'sum')
+print('EQUAL? ', np.array_equal(img2, img3))
+
+test = np.zeros(img2.shape)
+y_max = img2.shape[0]
+x_max = img2.shape[1]
+
+print(img2)
+print(img3)
 
 # rast.create_ndvi()
 
-plt.imshow(img2)
-plt.show()
+# plt.imshow(img2)
+# plt.show()
 
 # indexing practice
 # arr = np.array([[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16]])
--- SlidingWindow.py
+++ SlidingWindow.py
@@ -10,55 +10,36 @@ class SlidingWindow:
     # assumption: img_array is 2-dimensional
     # assumption: img_array is black and white, i.e. binary
     # TODO throw errors if assumptions are not met
-    def analyze_number_one(self, img_array, operation):
-        start = time.time() # TODO remove time
+    def analyze_test(self, img_array, operation):
+        start = time.time()  # TODO remove time
         y_max = img_array.shape[0]
         x_max = img_array.shape[1]
         array = np.array(img_array)
-       
-       # calculate sliding window for each value of delta
-       # delta is in powers of 2
-        for x in range(6):
-            delta = 2**x
-            array = array.flatten()
-            size = array.shape[0]
+        max_delta_power = 5
 
-            # create 4 offset arrays such that element wise operations operate on the four corners of a square of size delta+1
-            # first array is truncated, second array is offset for top right corner, third for bottom left, fourth for bottom right
-            # if that makes sense
-            array1 = array[0: size - (delta*x_max + delta)]
-            array2 = array[delta: size - (x_max*delta)]
-            array3 = array[delta*x_max: size - (delta)]
-            array4 = array[delta*x_max + delta: size]
-
-            # operate on four arrays
-            if operation.upper() == 'SUM':
-                array = array1 + array2 + array3 + array4
-            elif operation.upper() == 'MAX':
-                array = np.maximum(np.maximum(np.maximum(array1, array2), array3), array4)
-            else:
-                raise Exception('not valid operation')
-            
-            # pad to reshape into two dimensional array
-            array = np.pad(array, delta, 'constant')[delta:]
-            array = np.reshape(array, (y_max-delta, x_max))
-            # truncate last delta rows, which cannot create full delta+1 cubes
-            array = np.delete(array, np.s_[-delta:], 1)
-
-            # array has shrunk
-            x_max -= delta
+        for i in range(max_delta_power+1):
+            delta = 2**i
             y_max -= delta
+            x_max -= delta
+
+            new_array = np.zeros((y_max, x_max))
+            for y in range(y_max):
+                for x in range(x_max):
+                    sum = array[y, x] + array[y, x+delta] + array[y+delta, x] + array[y+delta, x+delta]
+                    new_array[y, x] = sum
+
+            array = new_array
 
         end = time.time() # TODO remove time
-        print(end-start)
-    
+        print('BAD TIME: ', end-start)
+
         return array
+
 
     # assumption: img_array is 2-dimensional
     # assumption: img_array is black and white, i.e. binary
     # TODO throw errors if assumptions are not met
-    # .6 seconds faster than method one! (for a 170MB img)
-    def analyze_number_two(self, img_array, operation):
+    def analyze(self, img_array, operation):
         start = time.time()  # TODO remove time
         y_max = img_array.shape[0]
         x_max = img_array.shape[1]
@@ -98,6 +79,6 @@ class SlidingWindow:
         array = np.delete(array, np.s_[-pad_num:], 1)
 
         end = time.time() # TODO remove time
-        print(end-start)
+        print('GOOD TIME: ', end-start)
 
         return array
