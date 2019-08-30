2e51fa9d2f4dd67177aa0df3a2ab89d7abf57252
David Schwartz <dmschwartz@bis.midco.net>
2019-08-30 09:57:24 -0500

    removed NDVI class

--- NDVI.py
+++
@@ -1,7 +0,0 @@
-from SlidingWindow import SlidingWindow
-
-class NDVI(SlidingWindow):
-
-    def __init__(self, x_max, y_max, power_target):
-        SlidingWindow.__init__(self, x_max, y_max, power_target)
-
