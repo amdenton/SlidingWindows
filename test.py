b3ff0bcd62c55f5ac5f9ae0cf5a3b7e833a86025
davschwartz <dmschwartz@bis.midco.net>
2019-09-16 11:11:38 -0500

    add max test to aggregation test

--- test.py
+++ test.py
@@ -13,16 +13,23 @@ class TestSlidingWindow(unittest.TestCase):
         img = rasterio.open(self.test_path)
         arr = img.read(1).astype(float)[0:512, 0:512]
 
-        arr_good = slide_window._partial_aggregation(arr, 0, 7, 'sum')
+        arr_good_sum = slide_window._partial_aggregation(arr, 0, 7, 'sum')
+        arr_good_max = slide_window._partial_aggregation(arr, 0, 7, 'max')
 
-        arr_good_partial = slide_window._partial_aggregation(arr, 0, 2, 'sum')
-        arr_good_partial = slide_window._partial_aggregation(arr_good_partial, 2, 3, 'sum')
-        arr_good_partial = slide_window._partial_aggregation(arr_good_partial, 3, 7, 'sum')
+        arr_good_partial_sum = slide_window._partial_aggregation(arr, 0, 2, 'sum')
+        arr_good_partial_sum = slide_window._partial_aggregation(arr_good_partial_sum, 2, 3, 'sum')
+        arr_good_partial_sum = slide_window._partial_aggregation(arr_good_partial_sum, 3, 7, 'sum')
+        arr_good_partial_max = slide_window._partial_aggregation(arr, 0, 2, 'max')
+        arr_good_partial_max = slide_window._partial_aggregation(arr_good_partial_max, 2, 3, 'max')
+        arr_good_partial_max = slide_window._partial_aggregation(arr_good_partial_max, 3, 7, 'max')
 
-        arr_brute = slide_window._aggregation_brute(arr, 'sum', 7)
+        arr_brute_sum = slide_window._aggregation_brute(arr, 'sum', 7)
+        arr_brute_max = slide_window._aggregation_brute(arr, 'max', 7)
 
-        self.assertTrue(np.array_equal(arr_good, arr_brute))
-        self.assertTrue(np.array_equal(arr_good_partial, arr_brute))
+        self.assertTrue(np.array_equal(arr_good_sum, arr_brute_sum))
+        self.assertTrue(np.array_equal(arr_good_max, arr_brute_max))
+        self.assertTrue(np.array_equal(arr_good_partial_sum, arr_brute_sum))
+        self.assertTrue(np.array_equal(arr_good_partial_max, arr_brute_max))
 
     def test_regression(self):
         slide_window = SlidingWindow(self.test_path, BandEnum.rgbIr)
