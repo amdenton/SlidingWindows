add star image generator, fix slope formula

windowagg/sliding_window.py
@@ -8,6 +8,8 @@ import math
 import affine
 import re
 
+import matplotlib.pyplot as plt
+
 class SlidingWindow:
 
     # TODO create more tests
@@ -612,15 +614,15 @@ class SlidingWindow:
         self.__create_tif(arr, pixels_aggre=pixels_aggre, fn=fn)
 
     # generate image of aggregated slope values
-    def dem_slope(self):
-        slope = self.__slope()
+    def dem_slope(self, cell_width, cell_height):
+        slope = self.__slope(cell_width, cell_height)
         slope = _Utilities._arr_dtype_conversion(slope, np.uint16)
         pixels_aggre = self.__dem_pixels_aggre
         fn = os.path.splitext(self.__file_name)[0] + '_slope_w' + str(pixels_aggre) +'.tif'
         self.__create_tif(slope, pixels_aggre=pixels_aggre, fn=fn)
 
     # return array of aggregated slope values
-    def __slope(self):
+    def __slope(self, cell_width, cell_height):
         if (self.__dem_arr_dict['z'].size == 0):
             raise ValueError('Arrays must be initialized before calculating slope')
 
@@ -628,12 +630,14 @@ class SlidingWindow:
         transform = self.__img.profile['transform']
         map_width = math.sqrt(transform[0]**2 + transform[3]**2)
         map_height = math.sqrt(transform[1]**2 + transform[4]**2)
-        z, xz, yz = tuple (self.__dem_arr_dict[i] for i in ('z', 'xz', 'yz'))
-        xx = (pixels_aggre^2-1)/12
-        b0 = xz/xx
-        b1 = yz/xx
+        xz, yz = tuple (self.__dem_arr_dict[i] for i in ('xz', 'yz'))
 
-        slope = np.sqrt(np.power(b0,2)+np.power(b1,2))
+        slope_x = xz*12/(pixels_aggre**2 - 1)
+        slope_y = yz*12/(pixels_aggre**2 - 1)
+        mag = np.sqrt(np.power(slope_x, 2) + np.power(slope_y, 2))
+        len_opp = (np.power(slope_x, 2) + np.power(slope_y, 2)) / mag
+        len_adj = np.sqrt( ((cell_width*map_width)**2) + ((cell_height*map_height)**2) )
+        slope = np.arctan(len_opp/len_adj)
 
         return slope
 