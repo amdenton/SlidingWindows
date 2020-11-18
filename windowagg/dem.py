2d51496c6592972d67caf18fe786cec7d49f7556
David Schwartz <dmschwartz01@gmail.com>
2020-11-18 13:43:11 -0600

    rework dem formulae again

--- windowagg/dem.py
+++ windowagg/dem.py
@@ -9,28 +9,34 @@ import numpy as np
 
 # return array of aggregated slope values
 def slope(dem_data, pixel_width, pixel_height):
+    w = pixel_width
+    h = pixel_height
     agg_window_len = 2**dem_data.num_aggre
     xx = (agg_window_len**2 - 1) / 12
     xz = dem_data.xz()
     yz = dem_data.yz()
+    dir_mag = np.sqrt(xz**2 + yz**2)
 
     return (
         ( np.power(xz, 2) + np.power(yz, 2) )
         /
-        ( xx * ( (np.absolute(xz) * pixel_width) + (np.absolute(yz) * pixel_height) ) )
+        (xx * (np.abs((w * xz) / dir_mag) + np.abs((h * yz) / dir_mag)))
     )
 
 # return array of aggregated slope values
 def slope_angle(dem_data, pixel_width, pixel_height):
+    w = pixel_width
+    h = pixel_height
     agg_window_len = 2**dem_data.num_aggre
     xx = (agg_window_len**2 - 1) / 12
     xz = dem_data.xz()
     yz = dem_data.yz()
+    dir_mag = np.sqrt(xz**2 + yz**2)
 
     return np.arctan(
         ( np.power(xz, 2) + np.power(yz, 2) )
         /
-        ( xx * ( (np.absolute(xz) * pixel_width) + (np.absolute(yz) * pixel_height) ) )
+        (xx * (np.abs((w * xz) / dir_mag) + np.abs((h * yz) / dir_mag)))
     )
 
 # return array of aggregated angle of steepest descent, calculated as clockwise angle from north
@@ -46,18 +52,18 @@ def profile(dem_data, pixel_width=1, pixel_height=1):
     agg_window_len = 2**dem_data.num_aggre
     z, xz, yz, yyz, xxz, xyz = dem_data.arrays()
 
-    xxxxminusxx2 = (agg_window_len**4 - (5 * agg_window_len**2) + 4) / 180
+    xxxx = ((3 * agg_window_len**4) - (10 * agg_window_len**2) + 7) / 240
     xx = (agg_window_len**2 - 1) / 12
-    a00 = (xxz - (xx * z)) / xxxxminusxx2
+    a00 = (xxz - (xx * z)) / (xxxx - xx**2)
     a10 = xyz / (2 * xx**2)
-    a11 = (yyz - (xx * z)) / xxxxminusxx2
-    b0 = xz / xx
-    b1 = yz / xx
+    a11 = (yyz - (xx * z)) / (xxxx - xx**2)
+    dir_mag = np.sqrt(xz**2 + yz**2)
 
-    # directional derivative of the slope of the following equation
-    # in the direction of the slope, derived in mathematica
-    # a00(x*w)**2 + 2a10(x*w)(y*h) + a11(y*h)**2 + b0(x*w) + b1(y*h) + cc
-    profile = (2*(a11*(b1**2)*(h**4) + b0*(w**2)*(2*a10*b1*(h**2) + a00*b0*(w**2)))) / ((b1*h)**2 + (b0*w)**2)
+    profile = (
+        (2 * ((xz**2 * a00) + (2 * xz * yz * a10) + (yz**2 * a11)))
+        /
+        ((xz**2 + yz**2) * (np.abs((w * xz) / dir_mag) + np.abs((h * yz) / dir_mag)))
+    )
 
     return profile
 
@@ -68,19 +74,18 @@ def planform(dem_data, pixel_width=1, pixel_height=1):
     agg_window_len = 2**dem_data.num_aggre
     z, xz, yz, yyz, xxz, xyz = dem_data.arrays()
     
-    xxxxminusxx2 = (agg_window_len**4 - (5 * agg_window_len**2) + 4) / 180
+    xxxx = ((3 * agg_window_len**4) - (10 * agg_window_len**2) + 7) / 240
     xx = (agg_window_len**2 - 1) / 12
-    a00 = (xxz - (xx * z)) / xxxxminusxx2
+    a00 = (xxz - (xx * z)) / (xxxx - xx**2)
     a10 = xyz / (2 * xx**2)
-    a11 = (yyz - (xx * z)) / xxxxminusxx2
-    b0 = xz / xx
-    b1 = yz / xx
+    a11 = (yyz - (xx * z)) / (xxxx - xx**2)
+    dir_mag = np.sqrt(xz**2 + yz**2)
 
-    
-    # directional derivative of the slope of the following equation
-    # in the direction perpendicular to slope, derived in mathematica
-    # a00(x*w)**2 + 2a10(x*w)(y*h) + a11(y*h)**2 + b0(x*w) + b1(y*h) + cc
-    planform = (2*h*w*(a11*b0*b1*(h**2) - a10*(b1**2)*(h**2) + a10*(b0**2)*(w**2) - a00*b0*b1*(w**2))) / ((b1*h)**2 + (b0*w)**2)
+    planform = (
+        (2 * ((xz**2 * a00) - (2 * xz * yz * a10) + (yz**2 * a11)))
+        /
+        ((xz**2 + yz**2) * (np.abs((w * xz) / dir_mag) + np.abs((h * yz) / dir_mag)))
+    )
 
     return planform
 
@@ -89,18 +94,18 @@ def standard(dem_data, pixel_width=1, pixel_height=1):
     w = pixel_width
     h = pixel_height
     agg_window_len = 2**dem_data.num_aggre
-    z, xz, yz, yyz, xxz, xyz = dem_data.arrays()
+    z, xz, yz, yyz, xxz, _xyz = dem_data.arrays()
     
-    xxxxminusxx2 = (agg_window_len**4 - (5 * agg_window_len**2) + 4) / 180
+    xxxx = ((3 * agg_window_len**4) - (10 * agg_window_len**2) + 7) / 240
     xx = (agg_window_len**2 - 1) / 12
-    a00 = (xxz - (xx * z)) / xxxxminusxx2
-    a10 = xyz / (2 * (xx**2))
-    a11 = (yyz - (xx * z)) / xxxxminusxx2
-    b0 = xz / xx
-    b1 = yz / xx
+    a00 = (xxz - (xx * z)) / (xxxx - xx**2)
+    a11 = (yyz - (xx * z)) / (xxxx - xx**2)
+    dir_mag = np.sqrt(xz**2 + yz**2)
 
-    # (profile + planform) / 2
-    # derived in mathematica
-    standard = (a00*b0*(w**3)*(-b1*h + b0*w) + a11*b1*(h**3)*(b1*h + b0*w) + a10*h*w*((-b1**2)*(h**2) + 2*b0*b1*h*w + (b0**2)*(w**2))) / ((b1*h)**2 + (b0*w)**2)
+    standard = (
+        (2 * ((xz**2 * a00) + (yz**2 * a11)))
+        /
+        ((xz**2 + yz**2) * (np.abs((w * xz) / dir_mag) + np.abs((h * yz) / dir_mag)))
+    )
 
     return standard
