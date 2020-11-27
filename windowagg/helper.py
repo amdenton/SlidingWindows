f8c091c8b6e072376d67d3c69df24f09c8331cd7
David Schwartz <33384202+DavSchwartz@users.noreply.github.com>
2020-11-27 10:43:24 -0600

    handle image conversion when (high_bound == low_bound)

--- windowagg/helper.py
+++ windowagg/helper.py
@@ -114,6 +114,12 @@ def arr_dtype_conversion(arr_in, dtype=np.uint16, low_bound=None, high_bound=Non
         if (arr_max > high_bound):
             raise ValueError('Upper bound must be greater than all values')
 
-    maximum = dtype_max(dtype)
-    arr_out = ((arr_in - low_bound) / (high_bound - low_bound) * maximum).astype(dtype)
+    original_max = dtype_max(arr_in.dtype)
+    new_max = dtype_max(dtype)
+    if (high_bound == low_bound):
+        original_max = dtype_max(arr_in.dtype)
+        arr_out = arr_in / original_max * new_max
+    else:
+        arr_out = ((arr_in - low_bound) / (high_bound - low_bound) * new_max).astype(dtype)
+    
     return arr_out
