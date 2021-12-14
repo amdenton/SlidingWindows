9df911b45f03fec886d0f5c20bf7da7dd127c2b8
Anne Denton <anne.denton@ndsu.edu>
2021-12-14 16:28:27 -0600

    changes for DEM paper

--- windowagg/agg_ops.py
+++ windowagg/agg_ops.py
@@ -1,3 +1,11 @@
+"""
+Last updated on Tue Dec 14
+
+@authors: Anne Denton, David Schwarz, Rahul Gomes
+
+License information:
+https://opensource.org/licenses/GPL-3.0
+"""
 from enum import Enum
 
 class Agg_ops(Enum):
